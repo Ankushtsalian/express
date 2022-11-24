db7955266d92ae875c028510a05b129d99fd9ebf
Ankush <ankushtsalian@gmail.com>
2022-11-24 21:41:57 +0530

    test server1

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ const http = require("http");
 const { readFileSync } = require("fs");
 
 const homePage = readFileSync("./navbar-app/index.html");
+// const js = readFileSync("./navbar-app/index.html");
+const css = readFileSync("./navbar-app/styles.css");
 
 const server = http.createServer((req, res) => {
-  req.write(homePage);
-  req.end();
+  const url = req.url;
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write(homePage);
+    res.end();
+  } else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(css);
+    res.end();
+  }
 });
 
 server.listen(5000, () => console.log("start"));
