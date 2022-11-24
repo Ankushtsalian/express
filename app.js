54466583f2a730d24f94084c8cf79c81173ba5b2
Ankush <ankushtsalian@gmail.com>
2022-11-24 21:31:41 +0530

    test server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Express Tutorial')
+const http = require("http");
+const { readFileSync } = require("fs");
+
+const homePage = readFileSync("./navbar-app/index.html");
+
+const server = http.createServer((req, res) => {
+  req.write(homePage);
+  req.end();
+});
+
+server.listen(5000, () => console.log("start"));
