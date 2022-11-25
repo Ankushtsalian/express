2bc2302c469f2f72b8ce3930ceb1a3b1cba7b0e9
Ankush <ankushtsalian@gmail.com>
2022-11-25 10:11:17 +0530

    test server2

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-const homePage = readFileSync("./navbar-app/index.html");
-// const js = readFileSync("./navbar-app/index.html");
-const css = readFileSync("./navbar-app/styles.css");
-const js = readFileSync("./navbar-app/browser-app.js");
-const svg = readFileSync("./navbar-app/logo.svg");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(css);
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(svg);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(js);
-    res.end();
-  }
+const server = app.get("/", (req, res) => {
+  res.end("helloWORLD");
 });
 
 server.listen(5000, () => console.log("start"));
