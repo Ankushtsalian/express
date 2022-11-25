bc2690c3703dd035f5438d9d190c4fe38ff0d29b
Ankush <ankushtsalian@gmail.com>
2022-11-25 12:42:55 +0530

    test server2_express_COMPARED NODE revised use method

--- app.js
+++ app.js
@@ -3,25 +3,12 @@ const path = require("path");
 const app = express();
 
 const homePage = "./navbar-app/index.html";
-const homeStyles = "./navbar-app/styles.css";
-const homeImage = "./navbar-app/logo.svg";
-const homeLogic = "./navbar-app/browser-app.js";
+
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
   // res.send("<h1>HOME PAGE</h1>");
   res.sendFile(path.resolve(__dirname, homePage));
-});
-
-app.get("/styles.css", (req, res) => {
-  res.sendFile(path.resolve(__dirname, homeStyles));
-});
-
-app.get("/logo.svg", (req, res) => {
-  res.sendFile(path.resolve(__dirname, homeImage));
-});
-
-app.get("/browser-app.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, homeLogic));
 });
 
 app.all("*", (req, res) => {
