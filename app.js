dca7b5c98a6c02f2e29f103e6ab7e1f4d703be7b
Ankush <ankushtsalian@gmail.com>
2022-11-25 14:52:39 +0530

    server test with json

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require("express");
 const app = express();
 const { products } = require("./data");
 app.get("/", (req, res) => {
-  res.json(products);
+  // res.json(products);
+  res.send('<a href="/products">products</a>');
+});
+
+app.get("/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, image, name } = product;
+    return { id, image, name };
+  });
+  res.json(newProducts);
 });
 
 app.listen(5000, () => {
