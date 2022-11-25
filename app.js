591bd12ad0bb1310c002c5d90da78c375b0f716c
Ankush <ankushtsalian@gmail.com>
2022-11-25 10:25:06 +0530

    test server2_express

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.end("helloLD");
+  res.send("<h1>HOME PAGE</h1>");
+});
+app.get("/about", (req, res) => {
+  res.send("<h1>About</h1>");
+});
+app.all("*", (req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 app.listen(5000, () => console.log("start"));
