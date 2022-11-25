21315bd9c5d4738385cf0c12cd1679dcb718b0ee
Ankush <ankushtsalian@gmail.com>
2022-11-25 09:14:33 +0530

    test server1

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const { readFileSync } = require("fs");
 const homePage = readFileSync("./navbar-app/index.html");
 // const js = readFileSync("./navbar-app/index.html");
 const css = readFileSync("./navbar-app/styles.css");
+const js = readFileSync("./navbar-app/browser-app.js");
+const svg = readFileSync("./navbar-app/logo.svg");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
@@ -14,6 +16,14 @@ const server = http.createServer((req, res) => {
   } else if (url === "/styles.css") {
     res.writeHead(200, { "content-type": "text/css" });
     res.write(css);
+    res.end();
+  } else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(svg);
+    res.end();
+  } else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(js);
     res.end();
   }
 });
