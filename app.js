9ddc72e3da91da5e687fce7796a44a2f0488f9ca
Ankush <ankushtsalian@gmail.com>
2022-11-25 11:36:01 +0530

    test server2_express_COMPARED NODE

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
+const homePage = "./navbar-app/index.html";
+const homeStyles = "./navbar-app/styles.css";
+const homeImage = "./navbar-app/logo.svg";
+const homeLogic = "./navbar-app/browser-app.js";
+
 app.get("/", (req, res) => {
-  res.send("<h1>HOME PAGE</h1>");
+  // res.send("<h1>HOME PAGE</h1>");
+  res.sendFile(path.resolve(__dirname, homePage));
 });
-app.get("/about", (req, res) => {
-  res.send("<h1>About</h1>");
+
+app.get("/styles.css", (req, res) => {
+  res.sendFile(path.resolve(__dirname, homeStyles));
 });
+
+app.get("/logo.svg", (req, res) => {
+  res.sendFile(path.resolve(__dirname, homeImage));
+});
+
+app.get("/browser-app.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, homeLogic));
+});
+
 app.all("*", (req, res) => {
   res.status(404).send("Page Not Found");
 });
