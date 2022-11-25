432c1af00f45c547f4c10122130fd6583816321f
Ankush <ankushtsalian@gmail.com>
2022-11-25 14:31:37 +0530

    server test with json

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-
-const homePage = "./navbar-app/index.html";
-
-app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-//   // res.send("<h1>HOME PAGE</h1>");
-//   res.sendFile(path.resolve(__dirname, homePage));
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("Page Not Found");
+const { products } = require("./data");
+app.get("/", (req, res) => {
+  res.json(products);
 });
 
-app.listen(5000, () => console.log("start"));
+app.listen(5000, () => {
+  console.log("Server is listening on port 5000....");
+});
