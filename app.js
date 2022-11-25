644ec5fd56d4ead77a42485bddeb1b24531519ed
Ankush <ankushtsalian@gmail.com>
2022-11-25 21:05:11 +0530

    server test with middleware logger

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
+const logger = require("./logger");
 
-app.get("/", (req, res) => {
-  res.send(
-    '<a href="/products/1">PRODUCT 1</a><br/><a href="/products/2">PRODUCT 2</a><br/><a href="/products/3">PRODUCT 3</a><br/>'
-  );
-});
-
-app.get("/products/:productId", (req, res) => {
-  const { productId } = req.params;
-
-  const newProduct = products.find(
-    (product) => product.id === Number(productId)
-  );
-
-  if (!newProduct) res.status(404).send("Not FOUND");
-  res.json(newProduct);
-});
-
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit } = req.query;
-  let allProducts = [...products];
-  if (search) {
-    allProducts = allProducts.filter((product) =>
-      product.name.startsWith(search)
-    );
-  }
-
-  if (limit) {
-    allProducts = allProducts.slice(0, Number(limit));
-  }
-
-  if (allProducts.length < 1) {
-    // res.send("<h1>No product found");
-    return res.status(200).json({ success: true, data: [] });
-  }
-
-  res.status(200).json(allProducts);
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
 app.all("*", (req, res) => {
